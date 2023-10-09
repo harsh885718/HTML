cf4f77e28b3e98bc8c3197c2028d864d8e232e0d
GadheDharmesh <8918dharam@gmail.com>
2023-10-09 19:34:43 +0530

    task

---
+++ task/css/java.js/java.js
@@ -0,0 +1,17 @@
+ul = document.getElementById('menu')
+bar = document.getElementById('bar')
+main = document.getElementById('main')
+
+
+
+function submenu() 
+{
+if(ul.style.display == "none" )
+{
+ul.style.display = "block" ;
+}
+else
+{
+ul.style.display = "none" ;
+}
+}
